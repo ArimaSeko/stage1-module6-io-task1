0f75d3625805331da124dd25d5ac60910e9b5014
ArimaSeko <120338228+ArimaSeko@users.noreply.github.com>
2023-04-12 01:31:28 +0600

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,58 +1,48 @@
 package com.epam.mjc.io;
 
 import java.io.*;
-import java.util.logging.Logger;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        
-        int Age ;
-        long Phone;
+        int Age=0;
+        long Phone=0;
         String Name;
         String Email;
-        boolean wr = false;
-        int startRow =0;
-        int endRow=0;
-        char[] charr=null;
-        try (FileInputStream fis = new FileInputStream(file)){
-            charr = new char[(int) file.length()];
-            int ch =fis.read();
-            int i =0;
-            while(ch!=-1){
-                charr[i]=(char)ch;
-                ch=fis.read();
+        char[] charArray = null;
+        try (FileInputStream fis = new FileInputStream(file)) {
+            charArray = new char[(int) file.length()];
+            int ch = fis.read();
+            int i = 0;
+            while (ch != -1) {
+                charArray[i] = (char) ch;
+                ch = fis.read();
                 i++;
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String data = String.valueOf(charr);
-        Profile prof = new Profile();
-        String strAge = cut(data,"Age: ");
-        Email = cut(data,"Email: ");
-        String strPhone = cut(data,"Phone: ");
-        Name = cut(data,"Name: ");
+        String data ="";
+        if(charArray!=null)data = String.valueOf(charArray);
+        String strAge = cut(data, "Age: ");
+        Email = cut(data, "Email: ");
+        String strPhone = cut(data, "Phone: ");
+        Name = cut(data, "Name: ");
+        try{
         Age = Integer.parseInt(strAge.trim());
-        Phone=Long.parseLong(strPhone.trim());
-        prof.setEmail(Email);
-        prof.setName(Name);
-        prof.setPhone(Phone);
-        prof.setAge(Age);
-        return prof;
+        Phone = Long.parseLong(strPhone.trim());}
+        catch (NumberFormatException e){e.printStackTrace();}
+        return new Profile(Name,Age,Email,Phone);
     }
-    public static String cut(String text, String find){
-        String answer="";
+
+    public static String cut(String text, String find) {
+        String answer = "";
         int start = text.indexOf(find);
-        text=text.substring(start,text.length());
+        text = text.substring(start);
         start = text.indexOf(find);
         int end = text.indexOf("\n");
-        if(start>-1)
-            answer =text.substring(start+find.length() ,end);
-       
+        if (start > -1)answer = text.substring(start + find.length(), end);
         return answer;
     }
 }
